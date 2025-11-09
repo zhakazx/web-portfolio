a8aa3794707014ca477f68171e666894f5f75bb0
Zhaka Hidayat <zhakazx@gmail.com>
2025-11-09 21:54:03 +0800

    chore: setup nitro/vite plugin for deployment (better compability with tanstack)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
@@ -12,6 +13,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 })
